345c2c83891a6d03d5596b4ec87e5c9564b69341
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-01-27 21:21:05 +0530

    Create Brain of Dec_Bin.js

---
+++ Brain of Dec_Bin.js
@@ -0,0 +1,21 @@
+
+
+let decToBin = () => {
+    let dec_str = document.getElementById("input_dec").value;
+    let num = Number(dec_str);
+    let arr = [];
+    for (let i = 0; num !== 0; i++) {
+        arr[i] = num % 2;
+        num = Math.floor(num / 2);
+    }
+    let arr2 = [];
+    let j = 0;
+    for(let i = arr.length; i>=0;i--) {
+        arr2[j] = arr[i];
+        j++;
+    }
+
+    let string_arr = arr2.toString();
+    arr_num = string_arr.split(",").join("");
+    document.getElementById("main_op").innerHTML = arr_num;
+}
