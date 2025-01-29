4577f8769653f3282886d434b52a84ba3f6b4760
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-01-29 22:35:48 +0530

    Create brain of dec_oct.js

---
+++ brain of dec_oct.js
@@ -0,0 +1,25 @@
+
+
+let decToBin = () => {
+    let dec_str = document.getElementById("input_dec").value;
+    let num = Number(dec_str);
+    let arr = [];
+    for (let i = 0; num !== 0; i++) {
+        arr[i] = num % 8;
+        num = Math.floor(num / 8);
+    }
+    let arr2 = [];
+    let j = 0;
+    for(let i = arr.length; i>=0;i--) {
+        arr2[j] = arr[i];
+        j++;
+    }
+
+    let string_arr = arr2.toString();
+    arr_num = string_arr.split(",").join("");
+    document.getElementById("main_op").innerHTML = arr_num;
+    let css1 = document.getElementById("main_op");
+    css1.style.transition = "all 0.5s";
+    css1.style.boxShadow = "2px 2px 5px black";
+
+}
